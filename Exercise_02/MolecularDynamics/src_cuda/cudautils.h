738d85e0dccb7aba1cfed071665690365539f96b
Arjun Lenan Sandhya <arjunhornet@gmail.com>
2026-05-20 12:11:17 +0200

    removed old files

--- Exercise_02/MolecularDynamics/src_cuda/cudautils.h
+++
@@ -1,9 +0,0 @@
-
-
-#define checkCudaError(ans) check((ans),__FILE__,__LINE__)
-inline void check(cudaError_t err, const char *file, int line){
-    if (err != cudaSuccess){
-        std::cerr<<"CUDA Runtime error at: "<<file<<":"<<line<<std::endl;
-        std::cerr<<cudaGetErrorString(err)<<" "<<file<<std::endl;
-    }
-}
